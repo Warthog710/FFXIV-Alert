c4900b6f9a7be2d0efbbb4deb1a3f9d367115b6f
Warthog710 <computergeek710@gmail.com>
2021-07-29 14:07:01 -0700

    Added TODO's

--- App.py
+++ App.py
@@ -1,7 +1,9 @@
-import atexit
-
 from flask import Flask, render_template, jsonify
 from Scraper import lodeStoneScraper
+
+#TODO: Add FFXIV Alarm title above buttons
+#TODO: Below title, and above buttons show icon legend
+#TODO: Below server cards show next update countdown.
 
 app = Flask(__name__)
 lss = lodeStoneScraper()
